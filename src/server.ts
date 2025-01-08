6c4490b000a9ba1e11114e4b66450a949b72ee4a
Daniel <vitordaniel2003g@gmail.com>
2025-01-08 17:32:59 -0300

    Criando server com express e adicionando devDependencies @types/express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log('ola');
+import express,{Request,Response} from "express"
+
+const app = express()
+
+app.get('/' ,(req: Request, res: Response) => {
+    console.log('fala pai');
+});
+
+app.listen(5557)
